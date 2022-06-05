e09231b156db707efb9e4fa91ca49bae2f77199b
YM-Studios <105734928+YM-Studios@users.noreply.github.com>
2022-06-05 12:16:29 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="노는방 " />
+        <Header title="노는방 홈페이지" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
